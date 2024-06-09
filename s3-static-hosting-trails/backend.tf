9f853e92f1755227482c22d30cc871c4963abf32
Clark Ritchie <clarkritchie@gmail.com>
2024-06-09 11:25:01 -0700

    rename this

--- s3-static-hosting-trails/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-clarkritchie"
-    key            = "s3-static-hosting-trails.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform_state"
-  }
-}
